b381855c266bc1c0d1732c3b5e290f801f8697b2
dtiwari1979 <36656171+dtiwari1979@users.noreply.github.com>
2021-06-01 12:08:47 +0530

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello - Jhooq-k8s message";
+        return "Hello - My Team";
     }
 }
